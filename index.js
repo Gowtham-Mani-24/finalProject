19feee2063764adca5a270ffc1a893b7dcbacacd
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-11-17 21:28:30 +0530

    database connected

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello Junga");
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('./utils/config');
+
+mongoose.connect(MONGODB_URI)
+    .then(() => console.log('Connected to Mongo Db'))
+    .catch(() =>console.error('not connected'));
