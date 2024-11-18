a295954026007feb5b6c4d8c0ccaace43542872a
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-11-18 19:27:08 +0530

    added server

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
-const { MONGODB_URI } = require('./utils/config');
+const { MONGODB_URI, PORT } = require('./utils/config');
+const app = require('./app');
 
 mongoose.connect(MONGODB_URI)
-    .then(() => console.log('Connected to Mongo Db'))
+    .then(
+        
+        () =>{
+            console.log('Connected to Mongo Db');
+
+            app.listen(PORT, () =>{
+                console.log('Server running on port 3001');
+            });
+        }
+    )
     .catch(() =>console.error('not connected'));
